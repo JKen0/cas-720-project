9965433a2741f50cbbba8a6e8db4c61166b76519
Ken <81273753+JKen0@users.noreply.github.com>
2022-12-16 10:05:16 -0500

    added submit button to the create policy page

--- front-end/src/components/CreatePolicy.js
+++ front-end/src/components/CreatePolicy.js
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import { Form, Field, FormElement } from "@progress/kendo-react-form";
 import { Error } from "@progress/kendo-react-labels";
 import { Input, NumericTextBox } from "@progress/kendo-react-inputs";
+import { Button, ButtonGroup } from "@progress/kendo-react-buttons";
 
 const CreatePolicy = () => {
   
@@ -11,35 +12,41 @@ const CreatePolicy = () => {
       <header style={{ padding: 10, fontSize: 25, fontWeight: "bold", }}>
         New Crop Insurance Policy
       </header>
-      <Form render={(formRenderProps) => (
-        <FormElement style={{ maxWidth: 650, paddingLeft: 10, paddingTop: 10 }}>
-            <legend className={"k-form-legend"}>
-              Please fill out the Insurance Policy Details: 
-            </legend>
-            <div className="mb-3">
-              <Field name={"ownerAddress"} component={Input} label={"Payable ETH Address"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"startDate"} component={Input} label={"Start Insurance Date"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"endDate"} component={Input} label={"End Insurance Date"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"location"} component={Input} label={"Location"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"minRain"} component={NumericTextBox} label={"Minimum Rain Amount"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"maxRain"} component={NumericTextBox} label={"Maximum Rain Amount"} />
-            </div>
-            <div className="mb-3">
-              <Field name={"insuredAmount"} component={NumericTextBox} label={"Insured Amount"} />
-            </div>
-        </FormElement>
-      )}
-    />
+      <div>
+        <Form render={(formRenderProps) => (
+          <FormElement style={{ maxWidth: 650, paddingLeft: 10, paddingTop: 10 }}>
+              <legend className={"k-form-legend"}>
+                Please fill out the Insurance Policy Details: 
+              </legend>
+              <div className="mb-3">
+                <Field name={"ownerAddress"} component={Input} label={"Payable ETH Address"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"startDate"} component={Input} label={"Start Insurance Date"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"endDate"} component={Input} label={"End Insurance Date"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"location"} component={Input} label={"Location"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"minRain"} component={NumericTextBox} label={"Minimum Rain Amount"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"maxRain"} component={NumericTextBox} label={"Maximum Rain Amount"} />
+              </div>
+              <div className="mb-3">
+                <Field name={"insuredAmount"} component={NumericTextBox} label={"Insured Amount"} />
+              </div>
+          </FormElement>
+          )}
+        />
+      </div>
+
+    <ButtonGroup>
+          <Button className = 'k-button k-button-solid k-button-solid-success'>Submit</Button>
+        </ButtonGroup>
     </React.Fragment>
   );
 }
